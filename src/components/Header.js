64bea442d76baf033baa53ca7d11318d2b94a19b
Merajul Islam <meraj.exe@gmail.com>
2023-10-11 18:20:12 +0600

    header done

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="bg-[#78b8cf] shadow-2xl h-20">
+      <div className="max-w-[1200px] h-full mx-auto">
+        <div className="w-full h-full flex items-center">
+          <h1 className="text-white text-2xl font-bold">ANIVALE </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
